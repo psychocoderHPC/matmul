329c7d496da736cbe30f73b3672e8960d0108ef5
René Widera <r.widera@hzdr.de>
2015-12-14 17:14:49 +0100

    use new alpaka interface

--- include/matmul/par/Alpaka.hpp
+++ include/matmul/par/Alpaka.hpp
@@ -998,14 +998,14 @@
 
 
         // Wrap the Pointers into memory buffer objects.
-        using BufWrapperIn = alpaka::mem::buf::ViewPlainPtr<
+        using BufWrapperIn = alpaka::mem::view::ViewPlainPtr<
             std::decay<decltype(devHost)>::type,
             TElem const,
             alpaka::dim::DimInt<2u>,
             TSize>;
         BufWrapperIn bufAHost(A, devHost, v2uiExtentsA, lda);
         BufWrapperIn bufBHost(B, devHost, v2uiExtentsB, ldb);
-        using BufWrapperOut = alpaka::mem::buf::ViewPlainPtr<
+        using BufWrapperOut = alpaka::mem::view::ViewPlainPtr<
             std::decay<decltype(devHost)>::type,
             TElem,
             alpaka::dim::DimInt<2u>,
